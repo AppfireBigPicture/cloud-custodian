99870f68fc22def6bed19c2f7ab46bad46ccba84
German <garces.g1999@gmail.com>
2025-03-19 10:52:11 +0100

    feat: add proper parameters management with .env and aws ssm parameter store

---
+++ docker/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+QUEUE_ARN=$(aws ssm get-parameter --name "/c7n/queue_arn" --with-decryption --query "Parameter.Value" --output text)
+export QUEUE_ARN
+SLACK_WEBHOOK_URL=$(aws ssm get-parameter --name "/c7n/slack_webhook_url" --with-decryption --query "Parameter.Value" --output text)
+export SLACK_WEBHOOK_URL
+# AWS_REGION and ROLE_ARN are defined in the .env file as they are not sensitive information.
+
+# Prepare config files for c7n
+envsubst < config/config.yaml.template > config/config.yaml
+
+
+exec "$@"
