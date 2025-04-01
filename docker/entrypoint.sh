911de27c96a026dbd5d61e141fafac5cf0ae1a45
German <garces.g1999@gmail.com>
2025-04-01 11:36:10 +0200

    refactor: refactor entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,14 +1,35 @@
 #!/bin/bash
 
-QUEUE_ARN=$(aws ssm get-parameter --name "/c7n/queue_arn" --with-decryption --query "Parameter.Value" --output text)
-export QUEUE_ARN
-SLACK_WEBHOOK_URL=$(aws ssm get-parameter --name "/c7n/slack_webhook_url" --with-decryption --query "Parameter.Value" --output text)
-export SLACK_WEBHOOK_URL
-# AWS_REGION and ROLE_ARN are defined in the .env file as they are not sensitive information.
+# Function to retrieve a parameter from AWS SSM
+get_ssm_parameter() {
+  local parameter_name=$1
+  aws ssm get-parameter --name "$parameter_name" --with-decryption --query "Parameter.Value" --output text
+}
 
-# Prepare config files for c7n
-envsubst < config/config.yaml.template > config/config.yaml
+# Function to prepare configuration files for Cloud Custodian
+prepare_config_files() {
+  envsubst < config/config.yaml.template > config/config.yaml
+}
 
-python scripts/policy_generator.py
+# Function to run the policy generator script
+run_policy_generator() {
+  python scripts/policy_generator.py
+}
 
-exec "$@"
+# Main function to execute the entrypoint logic
+main() {
+  QUEUE_ARN=$(get_ssm_parameter "/c7n/queue_arn")
+  export QUEUE_ARN
+  SLACK_WEBHOOK_URL=$(get_ssm_parameter "/c7n/slack_webhook_url")
+  export SLACK_WEBHOOK_URL
+  # AWS_REGION and ROLE_ARN are defined in the .env file as they are not sensitive information.
+
+
+  prepare_config_files
+  run_policy_generator
+
+  exec "$@"
+}
+
+# Execute the main function
+main "$@"
